19b327f3adc161a5e884f56852cf56a3b88f5eb1
homeboy12 <timofey1238kn@gmail.com>
2023-05-01 16:42:34 +0400

    F commit

---
+++ js/script.js
@@ -0,0 +1,29 @@
+function FoodMenu(){
+    let foodMenu = document.querySelector(".h1-cont");
+    let textfoodMenu = document.querySelector(".p-cont");
+    let imgfoodMenu = document.querySelector(".img-1");
+
+    foodMenu.innerHTML = "Food menu";
+    textfoodMenu.innerHTML = "Here is the food menu";
+    imgfoodMenu.src = "img/cafe4.jpg";
+}
+
+function DrinksMenu() {
+    let drinksdmenu = document.querySelector(".h1-cont");
+    let textDrinksMenu = document.querySelector(".p-cont");
+    let imgDrinksMenu = document.querySelector(".img-1");
+
+    drinksdmenu.innerHTML = "Drinks menu";
+    textDrinksMenu.innerHTML = "Here is the drinks menu";
+    imgDrinksMenu.src = "img/cafe2.jpg";
+}
+
+function TakeawayMenu() {
+    let takeawayMenu = document.querySelector(".h1-cont");
+    let textTakeawayMenu = document.querySelector(".p-cont");
+    let imgTakeawayMenu = document.querySelector(".img-1");
+
+    takeawayMenu.innerHTML = "Takeaway menu";
+    textTakeawayMenu.innerHTML = "Here is the Takeaway menu";
+    imgTakeawayMenu.src = "img/cafe3.jpg";
+}
